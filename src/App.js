3e6357ec25a2d5bfd4f3390dd341cbb51bb8eeea
robester0403 <robkso@gmail.com>
2022-12-03 19:56:35 -0500

    Render current location in a leaflet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setCurrentLocation([latitude, longitude]);
+  };
+
+  const error = () => {
+    console.log("Unable to retrieve your location");
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      {currentLocation && (
+        <MapContainer
+          style={{ height: "100vh", width: "100vh" }}
+          center={currentLocation}
+          zoom={13}
+          scrollWheelZoom={false}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </>
   );
 }
 
